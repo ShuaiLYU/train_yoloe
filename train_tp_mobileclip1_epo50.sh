8b6e2bb9f70e69ceee45bb0b59161f7072869396
ShuaiLYU <shuai.lyu@foxmail.com>
2025-10-18 12:21:08 +0800

    train_tp_mobileclip1_epo50.sh

---
+++ train_tp_mobileclip1_epo50.sh
@@ -0,0 +1,55 @@
+
+
+
+
+
+# Initialize conda for bash shell
+source ~/miniconda3/etc/profile.d/conda.sh
+# Activate ultralytics environment
+conda activate clipenv
+
+
+clip_weight_name="mobileclip:blt" # mobileclip2b
+
+
+for model in 11s
+do
+    lr=0.002
+
+    timestamp=$(date +%Y%m%d_%H%M%S)
+
+    run_dir="runs"
+    run_dir=$(realpath $run_dir)
+
+
+    project_name=yoloe_original_train_tp
+    project_dir=${run_dir}/${project_name}
+    mkdir -p $project_dir
+    exp_name=${clip_weight_name}_${model}_${lr}_close2_ep50_exp
+    exp_dir=${project_dir}/${exp_name}
+    echo "Experiment directory: $exp_dir"
+    mkdir -p $exp_dir
+    log_files="${exp_dir}-output.log"
+    echo "Log files: $log_files"
+
+
+    nohup python train_yoloe/train_tp.py \
+        --model_version $model \
+        --lr $lr \
+        --epochs 50 \
+        --close_mosaic 2 \
+        --batch 128 \
+        --device 0,1,2,3 \
+        --project $project_dir \
+        --name $exp_name \
+        --clip_weight_name $clip_weight_name \
+        > $log_files 2>&1 &
+
+        echo "using the following command to check the log:\n tail -f -n 50 $log_files"
+done
+
+
+
+
+# todo. batch   close amp 
+# --- IGNORE ---

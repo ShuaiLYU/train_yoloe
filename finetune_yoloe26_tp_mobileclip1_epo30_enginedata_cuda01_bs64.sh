e8355cd9497779d93b70f8f445f7011d5bc018da
ShuaiLYU <shuai.lyu@foxmail.com>
2025-11-07 23:39:20 +0800

    add train_yoloe/finetune_yoloe26_tp_mobileclip1_epo30.sh

---
+++ finetune_yoloe26_tp_mobileclip1_epo30_enginedata_cuda01_bs64.sh
@@ -0,0 +1,60 @@
+
+\
+
+
+
+# Initialize conda for bash shell
+source ~/miniconda3/etc/profile.d/conda.sh
+# Activate ultralytics environment
+conda activate clipenv
+
+
+
+
+
+# for model in 26s; do
+
+
+model=26s
+lr=0.002
+epo=30
+close_mosaic=5
+batch_size=64
+clip_weight_name="mobileclip:blt" # mobileclip2b
+timestamp=$(date +%Y%m%d_%H%M%S)
+
+run_dir="runs"
+
+ptw="object365v1" 
+
+project_name=yoloe26s_tp
+project_dir=${run_dir}/${project_name}
+mkdir -p $project_dir
+exp_name=${clip_weight_name}_${model}_bs${batch_size}_ptw${ptw}_cls${close_mosaic}_exp
+exp_dir=${project_dir}/${exp_name}
+echo "Experiment directory: $exp_dir"
+mkdir -p $exp_dir
+log_files="${exp_dir}-output.log"
+timestamp=$(date +%Y%m%d_%H%M%S)
+log_files="./runs/$timestamp.log"
+echo "Log files: $log_files"
+
+nohup python train_yoloe/finetune_yoloe26_tp.py \
+    --model_version $model \
+    --lr $lr \
+    --epochs $epo \
+    --close_mosaic $close_mosaic \
+    --batch $batch_size \
+    --device 0,1 \
+    --project $project_dir \
+    --name $exp_name \
+    --clip_weight_name $clip_weight_name \
+    > $log_files 2>&1 &
+
+echo "using the following command to check the log:\n tail -f -n 50 $log_files"
+
+
+
+
+# todo. batch   close amp 
+# --- IGNORE ---
